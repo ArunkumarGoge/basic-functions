b68c4f7ff924d16ba7fc391bbbc5f19a6a4d3fde
Arunkumar <ArunkumarGoge@users.noreply.github.com>
2015-05-26 15:46:22 +0530

    Update setstaticip.sh
    setting up static or dynamic ip for multiple interfaces

--- setstaticip.sh
+++ setstaticip.sh
@@ -1,64 +1,75 @@
 #!/bin/bash
 #This works only for ubuntu
 
+read -p "How many network interface do you want to configure? :" n
 
+echo auto lo eth0 > ./temp
+echo iface lo inet loopback >> ./temp
 
-	read -p "Do you want to set static ip or dynamic ip? static/dynamic:" prompt
-	if  [[ $prompt == "static" || $prompt == "Static" || $prompt == "s" || $prompt == "S" ]]
-	then
+	for ((i=0; i <n ; i++))
+	do
+		read -p "Do you want to set static ip or dynamic ip for Interface $((i+1))? static/dynamic:" prompt
+		if  [[ $prompt == "static" || $prompt == "Static" || $prompt == "s" || $prompt == "S" ]]
+		then
 
-		###############################################################
-		#getting input from the user
-		###############################################################
-		read -p "enter the ip address:" ip
-		read -p "enter the subnet mask:" subnet
-		read -p "enter the network ip:" netip
-		read -p "enter the broadcast ip:" bcip
-		read -p "enter the gateway ip:" gip
+			###############################################################
+			#getting input from the user
+			###############################################################
+			echo "enter the values for Interface$((i+1))"
+			read -p "enter the ip address:" ip
+			read -p "enter the subnet mask:" subnet
+			read -p "enter the network ip:" netip
+			read -p "enter the broadcast ip:" bcip
+			read -p "enter the gateway ip:" gip
 
-		###############################################################
-		#printing in the Terminal
-		################################################################
-		echo ip is $ip
-		echo subnet mask is $subnet
-		echo network ip is $netip
-		echo broadcast ip is $bcip
-		echo gateway is $gip
+			###############################################################
+			#printing in the Terminal
+			################################################################
+			echo ip is $ip
+			echo subnet mask is $subnet
+			echo network ip is $netip
+			echo broadcast ip is $bcip
+			echo gateway is $gipcd.
 
-		###############################################################
-		#single line to print the content to a file
-		###############################################################
-		#echo -e "auto lo eth0\niface lo inet loopback\niface eth0 inet static\naddress $ip\nnetmask $subnet\nnetwork $netip\nbroadcast $bcip\ngateway $gip" > /home/goge/network.conf
+			###############################################################
+			#single line to print the content to a file
+			###############################################################
+			#echo -e "auto lo eth0\niface lo inet loopback\niface eth0 inet static\naddress $ip\nnetmask $subnet\nnetwork $netip\nbroadcast $bcip\ngateway $gip" > /home/goge/network.conf
 
-		###############################################################
-		#lines to append the contents to the network file
-		###############################################################
-		echo auto lo eth0 > /etc/network/interfaces
-		echo iface lo inet loopback >> /etc/network/interfaces
-		echo iface eth0 inet static >> /etc/network/interfaces
-		echo address $ip >> /etc/network/interfaces
-		echo netmask $subnet >> /etc/network/interfaces
-		echo network $netip >> /etc/network/interfaces
-		echo broadcast $bcip >> /etc/network/interfaces
-		echo gateway $gip >> /etc/network/interfaces
-		echo dns-nameservers 8.8.8.8 >> /etc/network/interfaces
+			###############################################################
+			#lines to append the contents to the network file
+			###############################################################
+			
+			echo iface eth$i inet static >> ./temp
+			echo address $ip >> ./temp
+			echo netmask $subnet >> ./temp
+			echo network $netip >> ./temp
+			echo broadcast $bcip >> ./temp
+			echo gateway $gip >> ./temp
+			echo dns-nameservers 8.8.8.8 >> ./temp
 
-		###############################################################
-		#restarting the network services
-		###############################################################
-		sudo ifdown eth0 && sudo ifup eth0
+		elif [[ $prompt == "dynamic" || $prompt == "Dynamic" || $prompt == "d" || $prompt == "D" ]]
+		then
+			echo auto eth$i >> ./temp
+			echo iface eth$i inet dhcp >> ./temp
+			sudo ifdown eth$i && sudo ifup eth$i
 
-	elif [[ $prompt == "dynamic" || $prompt == "Dynamic" || $prompt == "d" || $prompt == "D"]]
-	then
-		echo auto lo eth0 > /etc/network/interfaces
-		echo iface lo inet loopback >> /etc/network/interfaces
-		echo auto eth0 >> /etc/network/interfaces
-		echo iface eth0 inet dhcp >> /etc/network/interfaces
-		sudo ifdown eth0 && sudo ifup eth0
+		else
+			echo "nothing has changed"
+		fi
+	done
 
-	else
-		echo "nothing has changed"
-	fi
+##################################################################
+#Writting the content to the files
+##################################################################
+cat temp > /etc/network/interfaces
+rm ./temp
 
-	
+###############################################################
+#restarting the network services
+###############################################################
+for ((i=0; i <n ; i++))
+do
+sudo ifdown eth$i && sudo ifup eth$i
+done
 
